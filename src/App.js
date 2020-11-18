8e8c032f2d8c406e9f53e500088975f9548e03da
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-18 18:24:49 +0500

    Update App.js
    imported AddTransaction.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { AccountSummary } from './components/AccountSummary';
 import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Balance />
         <AccountSummary />
         <TransactionHistory />
+        <AddTransaction />
       </div>
     </div>
   );
