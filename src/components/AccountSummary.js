a49d1b6ee5e5d7a7430de18630e252f749201c34
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-19 03:52:32 +0500

    Update AccountSummary.js
    Added functionality to account  summary on submitting it will automatically calculate total expense and income

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TransactionContext } from '../context/GlobalContext';
 
 export const AccountSummary = ()=> {
+
+    const { transactions } = useContext(TransactionContext);
+
+    const getIncome = ()=>{ 
+        let income = 0;
+        for( var i = 0; i < transactions.length; i++){
+            if(transactions[i].amount > 0){
+                income += transactions[i].amount
+            }
+        }
+        return income;
+    }
+
+    const getExpense = ()=>{
+        let expense = 0;
+        for( var i = 0; i < transactions.length; i++){
+            if(transactions[i].amount < 0){
+                expense += transactions[i].amount
+            }
+        }
+        return expense;
+    }
 
     return(
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
                 <p className="money plus">
-                    $480
+                    ${getIncome()}
                 </p>
             </div>
             <div>
                 <h4>Expense</h4>
                 <p className="money minus">
-                    $20
+                    ${getExpense()}
                 </p>
             </div>
         </div>
