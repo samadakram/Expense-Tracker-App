e5bceabad38f5826d06758cad9cdbfc45c822b44
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-19 02:19:44 +0500

    Update TransactionHistory.js
    added an array of transactions in which object having amount and desc

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -2,21 +2,26 @@ import React from 'react';
 
 export const TransactionHistory = () => {
 
+    let transactions = [
+        {desc: "Project 1 Income", amount: 500},
+        {desc: "Project 1 Salaries", amount: -100},
+        {desc: "Project 2 Income", amount: 1000},
+        {desc: "Project 2 Salaries", amount: -500},
+    ];
 
     return (
         <div>
             <h3>Transaction History</h3>
             <ul className="list">
-                <li className="plus">
-                    Project 1 income
-                    <span>$500</span>
+                {transactions.map((transObj, id)=>{
+                    return(
+                        <li className={transObj.amount < 0 ? "minus" : "plus"}>
+                    {transObj.desc}
+                    <span>${transObj.amount}</span>
                     <button className="delete-btn">X</button>
                 </li>
-                <li className="minus">
-                    Project 1 salaries
-                    <span>-$100</span>
-                    <button className="delete-btn">X</button>
-                </li>
+                    )
+                })}
             </ul>
         </div>
     );
