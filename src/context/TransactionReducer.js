3b77d75286f259ae00f315563eb22e85ca7744f2
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-19 04:19:31 +0500

    Update TransactionReducer.js
    added delete action

--- src/context/TransactionReducer.js
+++ src/context/TransactionReducer.js
@@ -3,6 +3,11 @@ const TransactionReducer = ((state, action)=>{
         case "ADD_TRANSACTION":{
             return [action.payload , ...state]
         }
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload) 
+            }
         default:
             return state;
     }
